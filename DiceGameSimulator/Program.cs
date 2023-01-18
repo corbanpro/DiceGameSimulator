01e4f80dcc0b6b6408f70b163d2ddea3da5ee69d
corbanpro <corbanpro@gmail.com>
2023-01-18 14:49:16 -0700

    added comments

--- DiceGameSimulator/Program.cs
+++ DiceGameSimulator/Program.cs
@@ -7,29 +7,36 @@ namespace DiceGameSimulator
         static void Main(string[] args)
         {
             Random rnd = new Random();
+
+            //declare variables
             int numRolls = 0;
-            int[] rollArray = new int[11];
+            int[] rollTotals = new int[11];
             string[] rollPercent = new string[11];
 
+            // Print game instructions
             Console.Write("Welcome to the dice throwing simulator! \n\nHow many dice rolls would you like to simulate? ");
             numRolls = Convert.ToInt32(Console.ReadLine());
 
+            //roll dice and fill rollTotals array with the data
             for (int i = 0; i < numRolls; i++)
             {
                 int roll1 = rnd.Next(1, 7);
                 int roll2 = rnd.Next(1, 7);
                 int sumRolls = roll1 + roll2;
 
-                rollArray[sumRolls - 2] = rollArray[sumRolls - 2] + 1;
+                rollTotals[sumRolls - 2]++;
             }
 
-            for (int i = 0; i < rollArray.Length; i++)
+            // determine what percent of rolls were each value 2-12
+            for (int i = 0; i < rollTotals.Length; i++)
             {
-                rollPercent[i] = new String('*', ((rollArray[i] * 100) / numRolls));
+                rollPercent[i] = new String('*', ((rollTotals[i] * 100) / numRolls));
             }
 
+            //print results of the dice roll simulation
             Console.WriteLine("\nDICE ROLLING SIMULATION RESULTS\nEach \" * \" represents 1 % of the total number of rolls.\nTotal number of rolls = " + numRolls + '\n');
-
+            
+            // print the asterisks
             for (int i = 0; i < rollPercent.Length; i++)
             {
                 Console.WriteLine((i + 2) + ": " + '\t' + rollPercent[i]);
